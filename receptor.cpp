b8cf805804923ec5f5a618fbdda079014e025cf7
Larry Fielding <larryjaguey@gmail.com>
2019-11-08 11:01:05 -0600

    main de receptor

--- receptor.cpp
+++ receptor.cpp
@@ -10,6 +10,32 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	int bytes_recv;
+	if (argc < 3)
+	{
+		cout << "Debe ingresar: " << endl;
+		cout << "./receptor ipMulticast puerto" << endl;
+		exit(1);
+	}
+
+    char ipRemota[50];
+    strcpy(ipRemota, argv[1]);
+	int puerto = atoi(argv[2]);
+
+	SocketMulticast socket  = SocketMulticast(puerto);
+	PaqueteDatagrama paquete_recv = PaqueteDatagrama(TAM_MAX_DATA);
+
+	socket.unirseGrupo(ipRemota);
+
+	while(1)
+	{
+		bytes_recv = socket.recibe(paquete_recv);
+
+		cout << "Bytes recibidos: " << bytes_recv << endl;
+        cout << "Cliente dijo: " << paquete_recv.obtieneDatos() << endl;
+        cout << "Recibido desde ip: " <<  ipRemota << endl;
+        cout << "Con puerto: " << puerto << endl;
+	}
+	
 	return 0;
 }
